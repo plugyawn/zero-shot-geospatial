6ff7088d21f2d0533e151db676b9091fb8e64796
Progyan <progyan.das@iitgn.ac.in>
2023-07-10 21:58:02 -0700

    check_llm.py: No grad!

--- code/CLIP/check_llm.py
+++ code/CLIP/check_llm.py
@@ -25,36 +25,35 @@ device = "cuda:0"  # replace "cpu" with "cuda" to use your GPU
 
 tokenizer = transformers.LlamaTokenizerFast.from_pretrained("hf-internal-testing/llama-tokenizer")
 model = transformers.LlamaForCausalLM.from_pretrained("decapoda-research/llama-7b-hf").to(device)
-
 classes = ["park", "desert", "parking lot", "open space"]
 
+with torch.no_grad():
+    batch = tokenizer(
+    text = f"""
+    I went to a book shop and got myself a...
+    """,
+    return_tensors="pt", 
+    add_special_tokens=False
+    )
 
-batch = tokenizer(
-text = f"""
-I went to a book shop and got myself a...
-""",
-return_tensors="pt", 
-add_special_tokens=False
-)
+    batch = {k: v.to(device) for k, v in batch.items()}
+    # Generate output from the model
+    output = model.generate(
+        input_ids=batch['input_ids'],
+        attention_mask=batch['attention_mask'],
+        max_new_tokens = 20,
+    )
 
-batch = {k: v.to(device) for k, v in batch.items()}
-# Generate output from the model
-output = model.generate(
-    input_ids=batch['input_ids'],
-    attention_mask=batch['attention_mask'],
-    max_new_tokens = 20,
-)
+    # Decode the output to text
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
-# Decode the output to text
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-print(generated_text)
+    print(generated_text)
 
 
-# Code translated from download.sh
-PRESIGNED_URL="" # replace with presigned url from email
-TARGET_FOLDER="./model/vi"
-model_sizes = ["7B"]
-N_SHARD_DICT = {
-    "7B": 0
-}
+    # Code translated from download.sh
+    PRESIGNED_URL="" # replace with presigned url from email
+    TARGET_FOLDER="./model/vi"
+    model_sizes = ["7B"]
+    N_SHARD_DICT = {
+        "7B": 0
+    }
